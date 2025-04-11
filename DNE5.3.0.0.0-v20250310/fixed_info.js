be1e48dfaf9fefb01fe6b1f60a9c7a0a30cab378
Daren <daren_dtrl@hotmail.com>
2025-04-11 13:03:02 +0800

    Add files via upload

--- DNE5.3.0.0.0-v20250310/fixed_info.js
+++ DNE5.3.0.0.0-v20250310/fixed_info.js
@@ -1,6 +1,6 @@
-var type_and_serial = "No serial number";
-var dne_version = "Please check each robot";
-var dne_option = "Not included in sample files";
+var type_and_serial = "RMS-SKIP-AAE01";
+var dne_version = "5.3.0.0.2";
+var dne_option = "D)U0S10C-V)SKIP/M32-!V)SKIP/IO76_DEFAULT";
 var mqtt_host = "";
 var mqtt_port = "";
 var mqtt_username = "";
