26434c8f999624bb077778df7b50813ad87f69da
Daren <daren_dtrl@hotmail.com>
2025-04-11 12:58:50 +0800

    Add files via upload

--- DNE5.3.0.0.0-v20250310/fixed_info.js
+++ DNE5.3.0.0.0-v20250310/fixed_info.js
@@ -1,6 +1,6 @@
-var type_and_serial = "RMS-10E2-N02";
-var dne_version = "DNE5.3.0.1.0";
-var dne_option = "F)VR2RRC-F)RRCEIO-F)VR2RRC/MT-F)RRCEIO/MT-P)NET3-P)NTP-P)DHCPD-P)SSM2-P)MQTT-P)WWW-P)BIAPP-F)JINV-T)CARRY-V)THOUZER-V)THOUZER/M23-V)THOUZER/S0C-D)U0LDSE310E";
+var type_and_serial = "No serial number";
+var dne_version = "Please check each robot";
+var dne_option = "Not included in sample files";
 var mqtt_host = "";
 var mqtt_port = "";
 var mqtt_username = "";
