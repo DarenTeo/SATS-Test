90f9c159a88310ce32b5c675cd2a30ff8c6ea69e
Daren <daren_dtrl@hotmail.com>
2025-05-15 09:59:32 +0800

    Add files via upload

--- DNE5.3.0.0.0-v20250310/fixed_info.js
+++ DNE5.3.0.0.0-v20250310/fixed_info.js
@@ -1,6 +1,6 @@
-var type_and_serial = "RMS-SKIP-AAE01";
-var dne_version = "DNE5.3.0.0.2";
-var dne_option = "F)VR2RRC-F)RRCEIO-F)VR2RRC/MT-F)RRCEIO/MT-P)NET3-P)NTP-P)DHCPD-P)SSM2-P)MQTT-P)WWW-P)BIAPP-F)JRIDE-T)CARRY-V)SKIP-D)U0S10C-V)SKIP/M32-V)SKIP/IO76_DEFAULT";
+var type_and_serial = "No serial number";
+var dne_version = "Please check each robot";
+var dne_option = "Not included in sample files";
 var mqtt_host = "";
 var mqtt_port = "";
 var mqtt_username = "";
